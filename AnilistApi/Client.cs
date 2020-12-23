65ba8c0a66da06b08072ecaab515e8d9ba781250
cainy-a <cainatkinson98@gmail.com>
2020-12-23 23:49:55 +0000

    Add project files.

---
+++ AnilistApi/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+
+namespace AnilistApi
+{
+	public class Client
+	{
+		public async Task<int> GetUserIdByUsername(string username)
+		{
+			var query         = "query ($username: String) { User (name: $username) { id } }";
+			var request = new GraphQLRequest{Query = query, Variables = new {username = username}};
+			var graphQlClient = new GraphQLHttpClient("https://graphql.anilist.co", new SystemTextJsonSerializer());
+
+			var response = await graphQlClient.SendQueryAsync<UserIdResponse>(request);
+
+			return response.Data.User.id;
+		}
+	}
+
+	public class UserIdResponse
+	{
+		public UserId User { get; set; }
+	}
+
+	public class UserId
+	{
+		public int id { get; set; }
+	}
+}
