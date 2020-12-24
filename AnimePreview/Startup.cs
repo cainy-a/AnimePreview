602bd91f0b96709cc226b70ae46b4d3fd2e1a7b5
cainy-a <cainy-a@users.noreply.github.com>
2020-12-24 11:44:12 +0000

    Fix it but actually this time

--- AnimePreview/Startup.cs
+++ AnimePreview/Startup.cs
@@ -21,7 +21,6 @@ namespace AnimePreview
 		{
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
